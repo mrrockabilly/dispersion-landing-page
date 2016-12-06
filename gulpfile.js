675ad5cba009d03bb5853a36af956559c7095c28
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-12-05 16:06:20 -0800

    modified gulp file to minimize css and html then compress images

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+var concat = require('gulp-concat');
+var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
+var htmlmin = require('gulp-htmlmin');
+
+gulp.task('minify', function() {
+  return gulp.src('*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', () =>
+    gulp.src('img/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/images'))
+);
+
+gulp.task('scripts', function(){
+    gulp.src('js/*.js')
+   // .pipe(concat('all.min.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist/js'))
+
+
+})
+
+gulp.task('minify-css', function() {
+  return gulp.src('css/*.css')
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('watch', function(){
+    gulp.watch('js/*.js',['scripts'])
+})
