cb14fe448d7928afb6be0f49eecb3b5f6924e71b
jrrubalc <jason.ray.rubalcaba@gmail.com>
2016-12-07 10:53:54 -0800

    create gulp defualt

--- gulpfile.js
+++ gulpfile.js
@@ -5,33 +5,40 @@ var cleanCSS = require('gulp-clean-css');
 var imagemin = require('gulp-imagemin');
 var htmlmin = require('gulp-htmlmin');
 
-gulp.task('minify', function() {
+gulp.task('minify', function () {
   return gulp.src('*.html')
-    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', () =>
-    gulp.src('img/*')
-        .pipe(imagemin())
-        .pipe(gulp.dest('dist/images'))
+gulp.task('compr-img', () =>
+  gulp.src('img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'))
 );
 
-gulp.task('scripts', function(){
-    gulp.src('js/*.js')
-   // .pipe(concat('all.min.js'))
+gulp.task('scripts', function () {
+  gulp.src('js/*.js')
+    // .pipe(concat('all.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('dist/js'))
-
-
 })
 
-gulp.task('minify-css', function() {
+gulp.task('minify-css', function () {
   return gulp.src('css/*.css')
-    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
     .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('watch', function(){
-    gulp.watch('js/*.js',['scripts'])
+gulp.task('default', function () {
+  gulp.run('minify', 'compr-img', 'scripts', 'minify-css');
+  gulp.task('watch', function () {
+    gulp.watch('js/*.js', ['scripts'])
+  })
+  gulp.watch('css/*.css', function (event) {
+    gulp.run('minify-css');
+  })
+  gulp.watch('*.html', function (event) {
+    gulp.run('minify');
+  })
 })
